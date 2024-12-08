fc0fab5509132ec18d0d6f0134977ccaeb272f82
Masaya Ohashi <m38.ohashi@gmail.com>
2024-12-08 11:18:45 +0900

    fix: PARTITION BY chair_id ORDER BY created_at のパフォーマンスを向上させるindexはる

--- webapp/sql/1-schema.sql
+++ webapp/sql/1-schema.sql
@@ -47,6 +47,7 @@ CREATE TABLE chair_locations
   PRIMARY KEY (id)
 )
   COMMENT = '椅子の現在位置情報テーブル';
+CREATE INDEX chair_locations_chair_id_created_at ON chair_locations (chair_id, created_at);
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users
