030e74bd4a20bcb2a7b8d255421c8636283102d1
hakshu25 <hakshu25@gmail.com>
2024-12-08 15:46:35 +0900

    Refactor notification endpoint in app_handler.rb

--- webapp/ruby/lib/isuride/app_handler.rb
+++ webapp/ruby/lib/isuride/app_handler.rb
@@ -308,57 +308,53 @@ module Isuride
 
     # GET /api/app/notification
     get '/notification' do
-      response = db_transaction do |tx|
-        ride = tx.xquery('SELECT * FROM rides WHERE user_id = ? ORDER BY created_at DESC LIMIT 1', @current_user.id).first
-        if ride.nil?
-          halt json(data: nil, retry_after_ms: 30)
+      ride = tx.xquery('SELECT * FROM rides WHERE user_id = ? ORDER BY created_at DESC LIMIT 1', @current_user.id).first
+      if ride.nil?
+        halt json(data: nil, retry_after_ms: 30)
+      end
+
+      yet_sent_ride_status = tx.xquery('SELECT * FROM ride_statuses WHERE ride_id = ? AND app_sent_at IS NULL ORDER BY created_at ASC LIMIT 1', ride.fetch(:id)).first
+      status =
+        if yet_sent_ride_status.nil?
+          get_latest_ride_status(tx, ride.fetch(:id))
+        else
+          yet_sent_ride_status.fetch(:status)
         end
 
-        yet_sent_ride_status = tx.xquery('SELECT * FROM ride_statuses WHERE ride_id = ? AND app_sent_at IS NULL ORDER BY created_at ASC LIMIT 1', ride.fetch(:id)).first
-        status =
-          if yet_sent_ride_status.nil?
-            get_latest_ride_status(tx, ride.fetch(:id))
-          else
-            yet_sent_ride_status.fetch(:status)
-          end
+      fare = calculate_discounted_fare(tx, @current_user.id, ride, ride.fetch(:pickup_latitude), ride.fetch(:pickup_longitude), ride.fetch(:destination_latitude), ride.fetch(:destination_longitude))
 
-        fare = calculate_discounted_fare(tx, @current_user.id, ride, ride.fetch(:pickup_latitude), ride.fetch(:pickup_longitude), ride.fetch(:destination_latitude), ride.fetch(:destination_longitude))
-
-        response = {
-          data: {
-            ride_id: ride.fetch(:id),
-            pickup_coordinate: {
-              latitude: ride.fetch(:pickup_latitude),
-              longitude: ride.fetch(:pickup_longitude),
-            },
-            destination_coordinate: {
-              latitude: ride.fetch(:destination_latitude),
-              longitude: ride.fetch(:destination_longitude),
-            },
-            fare:,
-            status:,
-            created_at: time_msec(ride.fetch(:created_at)),
-            updated_at: time_msec(ride.fetch(:updated_at)),
+      response = {
+        data: {
+          ride_id: ride.fetch(:id),
+          pickup_coordinate: {
+            latitude: ride.fetch(:pickup_latitude),
+            longitude: ride.fetch(:pickup_longitude),
           },
-          retry_after_ms: 30,
+          destination_coordinate: {
+            latitude: ride.fetch(:destination_latitude),
+            longitude: ride.fetch(:destination_longitude),
+          },
+          fare:,
+          status:,
+          created_at: time_msec(ride.fetch(:created_at)),
+          updated_at: time_msec(ride.fetch(:updated_at)),
+        },
+        retry_after_ms: 30,
+      }
+
+      unless ride.fetch(:chair_id).nil?
+        chair = tx.xquery('SELECT * FROM chairs WHERE id = ?', ride.fetch(:chair_id)).first
+        stats = get_chair_stats(tx, chair.fetch(:id))
+        response[:data][:chair] = {
+          id: chair.fetch(:id),
+          name: chair.fetch(:name),
+          model: chair.fetch(:model),
+          stats:,
         }
+      end
 
-        unless ride.fetch(:chair_id).nil?
-          chair = tx.xquery('SELECT * FROM chairs WHERE id = ?', ride.fetch(:chair_id)).first
-          stats = get_chair_stats(tx, chair.fetch(:id))
-          response[:data][:chair] = {
-            id: chair.fetch(:id),
-            name: chair.fetch(:name),
-            model: chair.fetch(:model),
-            stats:,
-          }
-        end
-
-        unless yet_sent_ride_status.nil?
-          tx.xquery('UPDATE ride_statuses SET app_sent_at = CURRENT_TIMESTAMP(6) WHERE id = ?', yet_sent_ride_status.fetch(:id))
-        end
-
-        response
+      unless yet_sent_ride_status.nil?
+        tx.xquery('UPDATE ride_statuses SET app_sent_at = CURRENT_TIMESTAMP(6) WHERE id = ?', yet_sent_ride_status.fetch(:id))
       end
 
       json(response)
